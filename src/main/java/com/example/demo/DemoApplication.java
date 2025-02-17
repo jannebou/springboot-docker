39cc12904216cea4fe8e4c2cd1a57c75801601cb
jannebou <Janne.sainio15@gmail.com>
2025-02-17 15:47:24 +0200

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -4,14 +4,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+/**
+ * Main application class. Can be extended to override behaviors.
+ */
 @SpringBootApplication
-public class DemoApplication {
-
-	public static void main(String[] args) {
+public final class DemoApplication {
+	/**
+	 * The entry point of the application.
+	 *
+	 * @param args command-line arguments
+	 */
+	public static void main(final String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+
+	/**
+	 * Home endpoint.
+	 * @return a welcome message
+	 */
 	@RequestMapping("/")
 	public String home() {
 		return "index";
